e0d37844ce02b6a6ef7804ea844e6584cb377d32
Christopher Lewis <>
2021-05-21 01:48:10 +0000

    I changed todo's 1.1a,b,c

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
@@ -14,7 +14,9 @@ let candidateAnswers;
 
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
+ /* // TODO 1.1b: Ask for candidate's name //*/
+    candidateName = input.question("What is your name?");
+    
 
 }
 
@@ -38,7 +40,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+console.log("Welcome to the quiz " + candidateName + " !");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
